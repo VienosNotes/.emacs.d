1ec3c5a64fd8c493ab5f5290bed4b236d059888f
D.Aoki <daoki@adscorp.jp>
2015-03-16 09:51:54 +0900

    add setting for Windows

--- conf/conf-font.el
+++ conf/conf-font.el
@@ -1,10 +1,26 @@
 (setq-default line-spacing 2)
 
-(set-face-attribute 'default nil :family "Monaco" :height 140)
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  (font-spec :family "Hiragino Kaku Gothic ProN"))
-(add-to-list 'face-font-rescale-alist
-             '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+;;OSX
+(when (eq system-type 'darwin)
+  (set-face-attribute 'default nil :family "Monaco" :height 140)
+  (set-fontset-font (frame-parameter nil 'font)
+		    'japanese-jisx0208
+		    (font-spec :family "Hiragino Kaku Gothic ProN"))
+  (add-to-list 'face-font-rescale-alist
+	       '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+  )
+
+;;Windows
+(when (eq system-type 'windows-nt)
+ (set-face-attribute 'default nil :family "Consolas" :height 140)
+  (set-fontset-font (frame-parameter nil 'font)
+  		    'japanese-jisx0208
+  		    (font-spec :family "Meiryo"))
+  (add-to-list 'face-font-rescale-alist
+  	       '("Meiryo" . 1.2))
+  
+ )
 
 (provide 'conf-font)
+
+
--- conf/conf-theme.el
+++ conf/conf-theme.el
@@ -1,3 +1,9 @@
-(require 'darcula-theme)
+(when (eq system-type 'darwin)
+  (require 'darcula-theme)
+  )
+
+(when (eq system-type 'windows-nt)
+  (require 'solarized-dark-theme)
+  )
 
 (provide 'conf-theme)
